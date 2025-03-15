69042daaf2b968faf9d8f14f5de2dfc86acc00c7
tranduc0102 <tranhuynhduc.vn@gmail.com>
2025-03-15 17:01:06 +0700

    Update Swap map
    Update Swap map

--- Assets/Extensions/New/Scripts/DragManager.cs
+++ Assets/Extensions/New/Scripts/DragManager.cs
@@ -21,9 +21,11 @@ namespace Extensions.New.Scripts
 
         public void OnBeginDrag(PointerEventData eventData)
         {
-            RaycastHit2D hit = Physics2D.Raycast(MouseWorldPosition(eventData.position), Vector2.down);
-            if (hit.transform.TryGetComponent(out DragItem item))
-            {
+            Vector3 position = eventData.position;
+            position.z = 0f;
+            RaycastHit2D hit = Physics2D.Raycast(MouseWorldPosition(position), Vector3.down);
+            if (hit && hit.transform.TryGetComponent(out DragItem item))
+            { 
                 currentItem = item;
                 currentItem.OnSelected();
             }
@@ -40,7 +42,7 @@ namespace Extensions.New.Scripts
             {
                 if (hit.transform.TryGetComponent(out DragItem item))
                 {
-                    SwapItem(currentItem, item);
+                    /*SwapItem(currentItem, item);*/
                 }
             }
         }
@@ -51,23 +53,31 @@ namespace Extensions.New.Scripts
             item1.OriginParent = item2.OriginParent;
             item2.OriginParent = tmpParent;
             item2.transform.SetParent(tmpParent);
-            ActionEndDrag();
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
-            ActionEndDrag();
+            ActionEndDrag(eventData.position);
         }
 
         public void OnPointerUp(PointerEventData eventData)
         {
-            ActionEndDrag();
+            ActionEndDrag(eventData.position);
         }
 
-        private void ActionEndDrag()
+        private void ActionEndDrag(Vector3 position)
         {
             if (currentItem)
             {
+                Vector3 pos = Camera.main.ScreenToWorldPoint(position);
+                RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.down);
+                if (hit)
+                {
+                    if (hit.transform.TryGetComponent(out DragItem item))
+                    {
+                        SwapItem(currentItem, item);
+                    }
+                }
                 currentItem.OnDrop();
                 currentItem = null;
             }
